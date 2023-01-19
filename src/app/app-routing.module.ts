0c88e7918e33c3d485462276f2d335c36394f015
Nensi-Rijal <nensirijal21@gmail.com>
2023-01-19 21:22:45 +0545

    feat: routing, and home page designs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/Home/Home.component';
 
 const routes: Routes = [
+  {path:'home',component:HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- src/app/components/Home/Home.component.ts
+++ src/app/components/Home/Home.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import {HttpClient} from "@angular/common/http";
+import { Router } from '@angular/router';
 
+const baseUrl = "../assets/foods.json";
 @Component({
   selector: 'app-Home',
   templateUrl: './Home.component.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  foodList: any;
+  constructor(private http: HttpClient,private router: Router) { }
+  
+  ngOnInit(): void {  
+    this.loadData();
+  }
 
-  ngOnInit() {
+  loadData(){
+    this.http.get(baseUrl).subscribe(data => {
+      this.foodList = data;
+      console.log(data);
+    })
+  }
+
+  onDetailClick(id: number){
+    this.router.navigateByUrl('/details/' + id);
   }
 
 }
